5fcc3be68e401399c0f6b2730e506ea211cc17fa
Eduwin Rojas <79290431+enrojass04@users.noreply.github.com>
2022-09-25 09:08:48 -0500

    Delete RoutesPolitecnico directory

--- RoutesPolitecnico/src/main/java/co/edu/poli/routespoli/repository/DriverRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.poli.routespoli.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.poli.routespoli.model.Driver;
-
-public interface DriverRepository extends JpaRepository<Driver, Integer> {
-
-}
--- RoutesPolitecnico/src/main/java/co/edu/poli/routespoli/repository/RouteRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.poli.routespoli.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.poli.routespoli.model.Route;
-
-public interface RouteRepository extends JpaRepository<Route, Integer> {
-
-}
