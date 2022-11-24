e1aaa58e4e66c32bcf7e9a92f3c71071a999177b
JaUrrego <79290502+JaUrrego@users.noreply.github.com>
2022-11-23 20:03:08 -0500

    Add files via upload

--- routesPoli/src/main/java/co/edu/poli/routes/model/User.java
+++ routesPoli/src/main/java/co/edu/poli/routes/model/User.java
@@ -9,34 +9,31 @@ import javax.persistence.Table;
 public class User {
 	@Id
 	private Integer id;
-	
+
+
 	private String password;
+
+
+
 	private String roles;
-		
-	public User(Integer id, String password, String roles) {
-		super();
-		this.id = id;
-		this.password = password;
-		this.roles = roles;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
+
+	
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	
 	public String getRoles() {
 		return roles;
 	}
+
 	public void setRoles(String roles) {
 		this.roles = roles;
 	}
-
 
 }
